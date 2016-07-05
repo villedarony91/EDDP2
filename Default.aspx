ebbb5ac273414b8b057a2155059a15c60d1a7317
wnk <rjlopez@DOMNTTSE>
2016-07-05 13:27:52 -0600

    login

---
+++ Default.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<%-- Agregue aquí los controles de contenido --%>
---
+++ Login.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web;
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void Unnamed_ServerClick(object sender, EventArgs e)
+    {
+        string s = user.Value;
+        string p = pass.Value;
+    }
+}
